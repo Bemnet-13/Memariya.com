fc9ca4b7f386b439a50f3ddb560d76eee0c80b51
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-01-02 17:38:24 +0300

    Home page finalized with tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/Courses.html", "./html/Home.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: '360px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        custom: "#9F7878",
+        customdark: "#714A4A",
+        gray: "#D9D9D9"
+      },
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans- serif'],
+      }
+    }
+  },
+  plugins: [],
+}
